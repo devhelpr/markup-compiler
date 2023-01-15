c5c6342d307fe8d34bd7dae46708fb5026431f4e
Maikel van de Lisdonk <maikel@devhelpr.com>
2023-01-15 18:03:41 +0100

    test with whitespace

--- markup-compiler/src/lib/markup-compiler.spec.ts
+++ markup-compiler/src/lib/markup-compiler.spec.ts
@@ -1,18 +1,25 @@
-
 import { compileMarkup } from './markup-compiler';
 
 describe('MarkupCompiler', () => {
   it('should return valid ast trees', () => {
-    expect(compileMarkup("<markup><test></test></markup>")).toBeTruthy();
-    expect(compileMarkup("<markup><test>hello</test></markup>")).toBeTruthy();
-    expect(compileMarkup("<markup><test>123hello</test></markup>")).toBeTruthy();
+    expect(compileMarkup('<markup><test></test></markup>')).toBeTruthy();
+    expect(compileMarkup('<markup><test>hello</test></markup>')).toBeTruthy();
+    expect(
+      compileMarkup('<markup><test>123hello</test></markup>')
+    ).toBeTruthy();
+    expect(
+      compileMarkup(`
+      <markup>
+        <test>123hello</test>
+      </markup>`)
+    ).toBeTruthy();
   });
 
   it('should not return valid ast trees', () => {
-    expect(compileMarkup("<markup></test>")).toBeFalsy();
-    expect(compileMarkup("<123markup></123markup>")).toBeFalsy();
-    expect(compileMarkup("<markup><test></test>")).toBeFalsy();
-    expect(compileMarkup("<markup><test>hello")).toBeFalsy();
-    expect(compileMarkup("<markup><test>123hello")).toBeFalsy();
+    expect(compileMarkup('<markup></test>')).toBeFalsy();
+    expect(compileMarkup('<123markup></123markup>')).toBeFalsy();
+    expect(compileMarkup('<markup><test></test>')).toBeFalsy();
+    expect(compileMarkup('<markup><test>hello')).toBeFalsy();
+    expect(compileMarkup('<markup><test>123hello')).toBeFalsy();
   });
 });
